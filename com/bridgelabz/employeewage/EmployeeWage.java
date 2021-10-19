c68009de7815ff91e36f34c03b4081e753612baa
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-19 18:27:18 +0530

    UC:3 Add Part Time Employee And Wage

--- com/bridgelabz/employeewage/EmployeeWage.java
+++ com/bridgelabz/employeewage/EmployeeWage.java
@@ -4,19 +4,42 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 
-		//Constants
+		//Constants declared
+
+		int Wage_Per_Day = 20;
+
+		int Full_Day_Hr = 8;
+
+		int Is_Part_Time = 2;
 
 		int Is_Present = 1;
 
-		//Computation
+		int Part_Day_Hr = 4;
 
-		double Emp_Check = Math.floor(Math.random() *10 )%2;
+		double Emp_Check = Math.floor(Math.random() *10 )%3;
 
-		if (Emp_Check == Is_Present )
+		//Conditional statement used to check present absent or parttime
 
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
+		if (Emp_Check == Is_Present ) {
 
+			int Emp_Wage = Wage_Per_Day * Full_Day_Hr;
+
+			System.out.println("Employee daily wage is " + Emp_Wage );
+
+		}
+		else if (Emp_Check == Is_Part_Time) {
+
+			int Emp_Wage = Wage_Per_Day * Part_Day_Hr;
+
+			System.out.println("Employee daily wage is " + Emp_Wage );
+
+		}
+
+		else {
+
+
+			System.out.println("Employee daily wage is 0 " );
+
+		}
 	}
 }
