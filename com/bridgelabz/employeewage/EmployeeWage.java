4cf6c4367ab27f3c7c883e593170a61b07ea25ca
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-19 18:08:08 +0530

    Display welcome message

---
+++ com/bridgelabz/employeewage/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+
+		//Prints message
+
+		System.out.println("Welcome to Employee Wage Computation Program !");
+
+	        }
+}
