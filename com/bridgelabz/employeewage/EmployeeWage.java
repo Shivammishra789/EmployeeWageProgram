bdfb4dfcf1a16a35b6285105eed23bf1bc2453fb
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-22 16:51:22 +0530

    compute emp wage using class method with function parameters

--- com/bridgelabz/employeewage/EmployeeWage.java
+++ com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,12 +1,37 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
-	
-	public static void main(String[] arg){
-		CalculateEmpWage emp1 = new CalculateEmpWage("tata motors",20,100,20);
-		System.out.println(emp1.toString());
-		CalculateEmpWage emp2 = new CalculateEmpWage("tata power",30,100,30);
-		System.out.println(emp2.toString());
-	}	
+
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	public static void computeEmpWage(String company, int empRatePerHour,
+									  int numOfWorkingDays, int maxHoursPerMonth) {
+		// Variables
+		int empHrs = 0, totalEmpHrs = 0, totalworkingDays = 0;
+		// Computation
+		while (totalEmpHrs <= maxHoursPerMonth &&					//checking till max condition
+				totalworkingDays < numOfWorkingDays) {
+			totalworkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {									   //random check for part time, full time, absent
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day: " + totalworkingDays +" Emp Hr: "+empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;			//calculating total employee wage
+		System.out.println("Total Emp Wage for Company "+company+" is: " + totalEmpWage);
+	}
+	public static void main(String[] args) {
+		computeEmpWage("Tata Motors", 45, 20, 100);        //calling method with parameter
+		computeEmpWage("Sinclair", 40, 20, 90);	
+	}
 }
-	
